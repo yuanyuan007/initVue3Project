498a41924984e6caaf59cdbdea8143258e72adce
muyuan <635410125@qq.com>
2019-10-31 13:47:16 +0800

    init project

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
@@ -21,10 +22,33 @@ module.exports = {
     }
   },
   chainWebpack: (config) => {
+    config.plugins.delete('prefetch'); //关闭预先加载模块
     config.resolve.alias
       .set('@$', resolve('src'))
       .set('assets', resolve('src/assets'))
       .set('components', resolve('src/components'))
-      .set('static', resolve('src/static'))
+      .set('static', resolve('src/static'));
+    if(process.env.NODE_ENV === 'production') {
+      if(process.env.npm_config_report) {
+        config
+          .plugin('webpack-bundle-analyzer')
+          .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+          .end();
+        config.plugins.delete('prefetch')
+      }
+    }
+  },
+  configureWebpack: (config) => {
+    if(process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+      config.mode = 'production'
+      return {
+        plugins: [new CompressionPlugin({
+          test: /\.js$|\.html$|\.css/, //匹配文件名
+          threshold: 10240, //对超过10k的数据进行压缩
+          deleteOriginalAssets: false //是否删除原文件
+        })]
+      }
+    }
   }
 }
